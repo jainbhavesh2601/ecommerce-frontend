af65d5ea0b71da31158e584bd6366d0a95a4660a
bhavesh_2601 <jainannu645@gmail.com>
2025-10-31 16:40:36 +0530

    fix: Add server configuration to Vite for development environment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true,  // 👈 This tells Vite to always serve index.html
+    port: 5173,                // (optional) your dev port
+  },
 })
